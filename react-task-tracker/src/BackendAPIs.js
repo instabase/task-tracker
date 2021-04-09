aec12c9c99a2f0d5d9e2429bee822cd15a842904
Chetan Mishra <chetan@instabase.com>
2021-04-09 11:11:21 -0700

    cleanup

--- react-task-tracker/src/BackendAPIs.js
+++ react-task-tracker/src/BackendAPIs.js
@@ -1,7 +1,3 @@
-const {GoogleAuth} = require('google-auth-library');
-const auth = new GoogleAuth();
-
-let googleAuthClient;
 const BACKEND_URL = 'https://appx-backend-cn4iaeqs6q-wn.a.run.app'
 const WEBAPP_URL = 'https://appx-webapp-cn4iaeqs6q-wn.a.run.app'
 
@@ -9,20 +5,13 @@ const WEBAPP_URL = 'https://appx-webapp-cn4iaeqs6q-wn.a.run.app'
 // This token is automatically handled by private Cloud Run (fully managed) and Cloud Functions.
 const renderRequest = async (route, serviceRequestOptions) => {
   const url = `${BACKEND_URL}/${route}`
-  console.log(`Sending request ${route} to ${BACKEND_URL}`)
   if (!('headers' in serviceRequestOptions)) {
     serviceRequestOptions.headers = {}
   }
   serviceRequestOptions.headers['Access-Control-Allow-Origin'] = WEBAPP_URL
   
-  try {
-    // serviceResponse converts the Markdown plaintext to HTML.
-    const serviceResponse = await fetch(url, serviceRequestOptions);
-    console.log('Received message from backend')
-    return serviceResponse;
-  } catch (err) {
-    throw Error('request to rendering service failed: ', err);
-  }
+  const serviceResponse = await fetch(url, serviceRequestOptions);
+  return serviceResponse;
 };
 
 export default renderRequest;
