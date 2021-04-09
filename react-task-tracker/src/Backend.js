30b0c0fa66c8ae80c7314e47a82faa46e0ea411f
Chetan Mishra <chetan@instabase.com>
2021-04-08 20:56:50 -0700

    added logging messages

--- react-task-tracker/src/Backend.js
+++ react-task-tracker/src/Backend.js
@@ -1,5 +1,4 @@
 const {GoogleAuth} = require('google-auth-library');
-const got = require('got');
 const auth = new GoogleAuth();
 
 let googleAuthClient;
@@ -9,7 +8,7 @@ const BACKEND_URL = 'https://appx-backend-cn4iaeqs6q-wn.a.run.app'
 // This token is automatically handled by private Cloud Run (fully managed) and Cloud Functions.
 const renderRequest = async (route, serviceRequestOptions) => {
   const url = `${BACKEND_URL}/${route}`
-
+  console.log(`Sending request ${route} to ${BACKEND_URL}`)
   try {
     // Create a Google Auth client with the Renderer service url as the target audience.
     if (!googleAuthClient) googleAuthClient = await auth.getIdTokenClient(url);
@@ -18,14 +17,16 @@ const renderRequest = async (route, serviceRequestOptions) => {
     const clientHeaders = await googleAuthClient.getRequestHeaders();
     serviceRequestOptions.headers['Authorization'] =
       clientHeaders['Authorization'];
+    console.log('Received gcloud auth token')
   } catch (err) {
     throw Error('could not create an identity token: ', err);
   }
 
   try {
     // serviceResponse converts the Markdown plaintext to HTML.
-    const serviceResponse = await got(url, serviceRequestOptions);
-    return serviceResponse.body;
+    const serviceResponse = await fetch(url, serviceRequestOptions);
+    console.log('Received message from backend')
+    return serviceResponse;
   } catch (err) {
     throw Error('request to rendering service failed: ', err);
   }
