4f267ec4bed04336997993d5ca4d35cff5643f09
Chetan Mishra <chetan@instabase.com>
2021-04-15 11:35:06 -0700

    CD should deploy to prod from branch

--- task-tracker-backend/setup.sh
+++ task-tracker-backend/setup.sh
@@ -1,3 +1,6 @@
-pyenv install 3.8.3
-pyenv global 3.8.3
+pyenv install 3.7.3
+pyenv global 3.7.3
 pip install -r requirements.txt
+
+# deps only needed for CD deployments 
+pip install jinja2==12.10 jinja2-cli==0.7.0
